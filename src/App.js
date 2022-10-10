692b0979a56c659117ed1c01be3ed78077e7a643
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-10-10 18:10:10 +0300

    added nav bar and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import NavbarComponent from './components/NavbarComponent';
+import './NavbarComponent.css';
 
 function App() {
   return (
     <div className="App">
-    
+      <NavbarComponent/>
     </div>
   );
 }
