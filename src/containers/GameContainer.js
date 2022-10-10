7261b34166d979c03f230fa0d4c07d2ead15ff7b
Carla022 <cs.morenol@outlook.com>
2022-10-10 17:00:34 +0100

    added  game container

---
+++ src/containers/GameContainer.js
@@ -0,0 +1,23 @@
+import Game from "../components/Game";
+import Lobby from "../components/Lobby";
+import Shop from "../components/Shop";
+import Battle from "../components/Battle";
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+const GameContainer = () => {
+    return (
+        <BrowserRouter>
+        <div>
+            <button>
+                <Link to="/Game">Play</Link>
+            </button>
+            <Routes>
+                <Route path="/Game" element={<Game />}/>
+                <Route path="/Lobby" element={<Lobby />}/>
+                <Route path="/Shop" element={<Shop />}/>
+                <Route path="/Battle" element={<Battle />}/>
+            </Routes>
+        </div>
+        </BrowserRouter>
+    )
+}
+export default GameContainer;
