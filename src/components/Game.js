2dd2ede41bdb47cd8e714ab82ba40881022668a9
Israt Jahan Ahmed <isratjahanahmed@israts-air.home>
2022-10-11 09:05:01 +0100

    game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,7 +6,7 @@ const Game = () => {
         <div className="StartGame">
             <form>
                 <input placeholder="Enter name"/>
-                <button> <Link to="/Lobby">Start Game</Link></button>
+                <button><Link to="/Lobby">Start Game</Link></button>
             </form>
         </div>
     )
