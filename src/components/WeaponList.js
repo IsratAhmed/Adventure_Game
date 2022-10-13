0792e8d22a44f6adfba1a8d24f23c2dd274c9966
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-10-13 19:00:47 +0300

    created weapon buttons

--- src/components/WeaponList.js
+++ src/components/WeaponList.js
@@ -11,8 +11,12 @@ const WeaponList = ({shop, buyWeapon}) => {
     })
 
     return (
-        <>        
-            {weaponItems}
+        <>   
+        <button onClick={()=> {buyWeapon(availableWeapons[0].id)}}>{availableWeapons[0].name}</button>     
+        <button onClick={()=> {buyWeapon(availableWeapons[1].id)}}>{availableWeapons[1].name}</button>     
+        <button onClick={()=> {buyWeapon(availableWeapons[2].id)}}>{availableWeapons[2].name}</button>     
+        <button onClick={()=> {buyWeapon(availableWeapons[3].id)}}>{availableWeapons[3].name}</button>     
+        {weaponItems}
         </>
     );
 
